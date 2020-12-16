896bbc766615d34376a3d4fca2484339e7879b6a
Suwijak Chaipipat <ppnk2537@gmail.com>
2020-12-16 16:55:23 +0700

    Structure project by adding components and assets directory

--- src/App.js
+++ src/App.js
@@ -1,49 +1,8 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import './App.css';
 import data from './data.json'
 import 'antd/dist/antd.css';
-import img from './shoe.png';
-
-const ProductCard = ({ item }) => {
-  const card = useRef(null)
-
-  const hello = () => {
-    card.current.classList.add('animate')
-  }
-
-  const helloOut = () => {
-    card.current.classList.remove('animate')
-  }
-
-  return (
-    <div className={'product-container'}>
-      <div className={`product-card ${item.stock === 0 ? 'product-out-of-stock' : ''}`} ref={card} onMouseOver={hello} onMouseLeave={helloOut}>
-        <img src={img} alt=""/>
-        <div className="add-to-cart-button">Add to Cart</div>
-        <div className="stats">
-          <div className="stats-container">
-            <span className="product_price">${ item.price }</span>
-            <span className="product_name">{ item.name }</span>
-            <p>{ item.description }</p>
-
-            <div className="product-options">
-              <strong>SIZES</strong>
-              <span>{ item.sizes.join(', ')}</span>
-              <strong>COLORS</strong>
-              <div className="colors">
-                {
-                  item.colors.map(i => (
-                    <div key={i} className={`c-${i}`}><span /></div>
-                  ))
-                }
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+import ProductCard from './components/ProductCard'
 
 function App() {
   return (
