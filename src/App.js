7c2b5ad3be0fe691b790ac5bb1da24d5ccb33fa5
Holly Moly <acebomoza@gmail.com>
2020-12-16 16:44:28 +0700

    Initial project.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react'
 import './App.css';
+import data from './data.json'
+import 'antd/dist/antd.css';
+import img from './shoe.png';
+
+const ProductCard = ({ item }) => {
+  const card = useRef(null)
+
+  const hello = () => {
+    card.current.classList.add('animate')
+  }
+
+  const helloOut = () => {
+    card.current.classList.remove('animate')
+  }
+
+  return (
+    <div className={'product-container'}>
+      <div className={`product-card ${item.stock === 0 ? 'product-out-of-stock' : ''}`} ref={card} onMouseOver={hello} onMouseLeave={helloOut}>
+        <img src={img} alt=""/>
+        <div className="add-to-cart-button">Add to Cart</div>
+        <div className="stats">
+          <div className="stats-container">
+            <span className="product_price">${ item.price }</span>
+            <span className="product_name">{ item.name }</span>
+            <p>{ item.description }</p>
+
+            <div className="product-options">
+              <strong>SIZES</strong>
+              <span>{ item.sizes.join(', ')}</span>
+              <strong>COLORS</strong>
+              <div className="colors">
+                {
+                  item.colors.map(i => (
+                    <div key={i} className={`c-${i}`}><span /></div>
+                  ))
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={'app-header'}>
+        <div>
+          <input type={'radio'} name={'status'} /><span>Select all</span>
+          <input type={'radio'} name={'status'} /><span>Only in stock</span>
+        </div>
+      </div>
+      <div className={'app-content'}>
+        <div id={'content'}>
+          {
+            data.map(item => (
+              <ProductCard key={item.id} item={item} />
+            ))
+          }
+        </div>
+      </div>
+
     </div>
   );
 }
