c6de10ce0bf8040ee3d4a754d01f0e78244ddfb5
Suwijak Chaipipat <ppnk2537@gmail.com>
2020-12-16 17:16:12 +0700

    Add product filter to App component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import data from './data.json'
 import 'antd/dist/antd.css';
+import { Radio } from 'antd'
 import ProductCard from './components/ProductCard'
 
 function App() {
+  const [selectedFilter, setSelectedFilter] = useState('Select all')
+
+  const onFilterChange = (event) => setSelectedFilter(event.target.value)
+
+  let products = data
+
+  if (selectedFilter === 'Only in stock') {
+    products = products.filter(product => product.stock !== 0)
+  }
+
   return (
     <div className="App">
       <div className={'app-header'}>
-        <div>
-          <input type={'radio'} name={'status'} /><span>Select all</span>
-          <input type={'radio'} name={'status'} /><span>Only in stock</span>
+        <div className={'header-content'}>
+          <Radio.Group
+            options={['Select all', 'Only in stock']}
+            onChange={onFilterChange}
+            value={selectedFilter}
+            optionType="button"
+            buttonStyle="solid"
+          />
         </div>
       </div>
       <div className={'app-content'}>
         <div id={'content'}>
           {
-            data.map(item => (
+            products.map(item => (
               <ProductCard key={item.id} item={item} />
             ))
           }
